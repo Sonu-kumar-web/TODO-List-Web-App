7470ca5da9129eaef34aa78c67fd6a3d4485f810
Sonu <sonu3660@gmail.com>
2020-07-15 19:07:05 +0530

    Edit Database name

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,12 +1,14 @@
 // Add library mongoose
-const mongoose=require('mongoose');
+const mongoose = require("mongoose");
 
 // Connect to database
-mongoose.connect('mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Todo?retryWrites=true&w=majority');
+mongoose.connect(
+   "mongodb+srv://sonu:sonu@free-cluster-t4e4s.mongodb.net/Todo_List?retryWrites=true&w=majority"
+);
 
 // check connection
-const db=mongoose.connection;
-db.on('error',console.error.bind(console,'error in connection to database'));
-db.once('open',function(){
-    console.log('DB connection success');
-})
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "error in connection to database"));
+db.once("open", function () {
+   console.log("DB connection success");
+});
